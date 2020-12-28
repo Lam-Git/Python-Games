fd6c798da2845791f1e7c38c28a2df16cc49f3fd
Lam Nguyen <lsnguyen0@gmail.com>
2020-12-27 21:37:18 -0800

    commit

--- python/guess_the_number(com)/num_game.py
+++ python/guess_the_number(com)/num_game.py
@@ -6,11 +6,11 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0 #Cannot be number 0
     while guess != random_number:
-        guess = input("Guess a number between 1 and {x}:")
+        guess = int(input(f"Guess a number between 1 and {x}:"))
         if guess < random_number:
             print("Sorry, guess again. Too low.")
         elif guess > random_number:
-            print("Sorry, guess again.Too high.")
+            print("Sorry, guess again. Too high.")
         
     print(f "Yep, congrats. You have guessed the number {random_number} correctly. ")
 
