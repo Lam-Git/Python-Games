99afd6410395d1fe4da92d52b9fdfa61f95dbf60
Lam Nguyen <lsnguyen0@gmail.com>
2020-12-28 16:44:29 -0800

    commit

--- hangman.py
+++ hangman.py
@@ -17,28 +17,46 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
 
+    lives = 8
+
     # getting user input
     # taking out the words that has been already used
-    while len(word_letters)>0
-    #letter used
-    print("You have used these letters:", "".join(used_letters))
-    #what the current word is (ie W-D-L)
-    word_list=[letter if letter in used_letters else "-" for letter in word]
-    print("Current word: ", " ".join(word_list))
-    user_letter = str(input("Guess a letter: ")).upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        print(
+            "You have",
+            lives,
+            "lives left and you have used these letters: ",
+            " ".join(used_letters),
+        )
 
-    elif user_letter in used_letters:
-        print("You have already used that character. Please try again.")
+        # what the current word is (ie W-D-L)
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
 
+        user_letter = str(input("Guess a letter: ")).upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print("")
+
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print("\nYour letter,", user_letter, "is not in the word.")
+
+        elif user_letter in used_letters:
+            print("\nYou have already used that letter. Guess another letter.")
+
+        else:
+            print("Invalid character. Please try again! ")
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+    if lives == 0:
+        print("You died, sorry. The word was", word)
     else:
-        print("Invalid character. Please try again! ")
+        print("YAY! You guessed the word", word, "!!")
 
-    #gets here when len(word_letters) ==0
 
-user_input = str(input("Type a letter: "))
-print(user_input)
-
+if __name__ == "__main__":
+    hangman()
