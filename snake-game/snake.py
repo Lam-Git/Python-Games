c58c9dbb529a208ebe927a85846512f8dcb2085d
Lam Nguyen <lsnguyen0@gmail.com>
2021-01-27 19:34:53 -0800

    commit

--- snake-game/snake.py
+++ snake-game/snake.py
@@ -20,8 +20,8 @@ wn.tracer(0)  # Turns off the screen updates
 
 # Head of the snake
 head = turtle.Turtle()
-head = shape("square")
-head = color("white")
+head.shape("turtle")
+head.color("white")
 head.penup()
 head.goto(0, 0)
 head.direction = "Stop"
@@ -31,11 +31,12 @@ food = turtle.Turtle()
 colors = random.choice(["red", "blue", "green"])
 shapes = random.choice(["square", "triangle", "circle"])
 food.speed(0)
-food.shape("circle")
+food.shape("triangle")  # this can be change
 food.color("red")
 food.penup()
 food.goto(0, 100)
 
+# Pen
 pen = turtle.Turtle()
 pen.speed(0)
 pen.shape("square")
@@ -43,26 +44,26 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 250)
-pen.write("Score : 0 High Score : 0", align="center", font=("candara", 24, "bold"))
+pen.write("Score : 0 High Score : 0", align="center", font=("Courier", 24, "bold"))
 
-
+# Function
 # assigning key directions
-def goup():
+def go_up():
     if head.direction != "down":
         head.direction = "up"
 
 
-def godown():
+def go_down():
     if head.direction != "up":
         head.direction = "down"
 
 
-def goleft():
+def go_left():
     if head.direction != "right":
         head.direction = "left"
 
 
-def goright():
+def go_right():
     if head.direction != "left":
         head.direction = "right"
 
@@ -94,6 +95,8 @@ segments = []
 # Main Gameplay
 while True:
     wn.update()
+
+    # Check for a collision with the border
     if (
         head.xcor() > 290
         or head.xcor() < -290
@@ -105,18 +108,26 @@ while True:
         head.direction = "Stop"
         colors = random.choice(["red", "blue", "green"])
         shapes = random.choice(["square", "circle"])
+
+        # High the segments
         for segment in segments:
             segment.goto(1000, 1000)
+
+        # Clear the segments list
         segments.clear()
+        # Reset the score
         score = 0
+        # Resent the delay
         delay = 0.1
         pen.clear()
         pen.write(
             "Score : {} High Score : {} ".format(score, high_score),
             align="center",
-            font=("candara", 24, "bold"),
+            font=("Courier", 24, "bold"),
         )
+    # Check for a collision with the food
     if head.distance(food) < 20:
+        # Move the food to a random spot
         x = random.randint(-270, 270)
         y = random.randint(-270, 270)
         food.goto(x, y)
@@ -124,11 +135,15 @@ while True:
         # Adding segment
         new_segment = turtle.Turtle()
         new_segment.speed(0)
-        new_segment.shape("square")
-        new_segment.color("orange")  # tail colour
+        new_segment.shape("square")  # tail shape
+        new_segment.color("gray")  # tail color
         new_segment.penup()
         segments.append(new_segment)
+
+        # Shorten the delay
         delay -= 0.001
+
+        # Increase the score
         score += 10
         if score > high_score:
             high_score = score
@@ -138,16 +153,20 @@ while True:
             align="center",
             font=("candara", 24, "bold"),
         )
-    # Checking for head collisions with body segments
+    # Move the end segments first in reverse order
     for index in range(len(segments) - 1, 0, -1):
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
         segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
         segments[0].goto(x, y)
     move()
+
+    # Check for head collision with the body segments
     for segment in segments:
         if segment.distance(head) < 20:
             time.sleep(1)
@@ -155,17 +174,26 @@ while True:
             head.direction = "stop"
             colors = random.choice(["red", "blue", "green"])
             shapes = random.choice(["square", "circle"])
+
+            # Hide the segments
             for segment in segments:
                 segment.goto(1000, 1000)
+
+            # Clear the segments list
             segment.clear()
 
+            # Reset the score
             score = 0
+
+            # Reset the delay
             delay = 0.1
+
+            # Update the score display
             pen.clear()
             pen.write(
                 "Score : {} High Score : {} ".format(score, high_score),
                 align="center",
-                font=("candara", 24, "bold"),
+                font=("Courier", 24, "bold"),
             )
     time.sleep(delay)
 
