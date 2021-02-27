92d10b3788ef4c455f5d7fdd7646f5fb88c5b99f
Lam Nguyen <lsnguyen0@gmail.com>
2021-02-27 12:33:26 -0800

    commit

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -20,6 +20,8 @@ num = input("Number: ")
 plural_noun4 = input("Plural noun: ")
 
 
+# The Story Board:
+
 madlib = f" A vacation is when you take a trip to some {adj1} place with your {adj2} family. Usually you go to some place that is near a/an {noun1} or up on a/an {noun2}. A good vacation place is one where you can ride {plural_noun1} or playing {game} or go hunting for {plural_noun2}. I like to spend my time {verb_ending_in_ing1} or {verb_ending_in_ing2}. When parents go on vacation, they spend their time eating three {plural_noun3} a day while fathers play {game1}, and mothers sit around {verb_ending_in_ing3}.  Last summer, my little brother fell in a/an {noun3} and got poison {plant} all over his {body_part}. My family is going to go to the {a_place}, and I will practice {verb_ending_in_ing4} Parents need vacations more than kids because parents are always very {adj3} and because they have to work {num} hours ever day all year making enough {plural_noun4} to pay for the vacation."
 
 print(madlib)
